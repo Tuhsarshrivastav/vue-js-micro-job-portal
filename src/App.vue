59b6d579d4ab195528f976fe7b4221e25dfbb600
Tusharshrivastav <ts3657@gmail.com>
2021-08-30 08:48:37 +0530

    build is complete

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+      <Navigation />
+      <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+export default {
+  components: { Navigation },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
